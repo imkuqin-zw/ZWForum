0151286fcff54d4c95082ab56cd0fa0cb8522838
1141137429 <1141137429@qq.com>
2017-03-10 17:56:18 +0800

    first commit

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
         $userInstance = User::where('github_id',$user->id)->orWhere('email',$user->email)->firstOrFail();
         Auth::login($userInstance);
 
-        return redirect()->route('user.show');
+        return redirect('/');
 
     }
 
@@ -88,7 +88,7 @@ class AuthController extends Controller
         $user = $this->user->create($data);
         Auth::login($user);
 
-        return redirect()->route('user.show');
+        return redirect('/');
     }
 
     /**
@@ -128,6 +128,6 @@ class AuthController extends Controller
         $this->user->update($user->id,$data['github_id']);
         Auth::login($user);
 
-        return redirect()->route('user.show');
+        return redirect('/');
     }
 }
